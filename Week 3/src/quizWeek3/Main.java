0f70db6b33f918d967eaf31f838afc39f0d8ecc2
Mark Baldwin <baldwin.mv05@gmail.com>
2019-05-08 12:54:49 -0600

    Add files via upload

---
+++ Week 3/src/quizWeek3/Main.java
@@ -0,0 +1,20 @@
+package quizWeek3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            Integer x = 100;
+            Integer y = 100;
+            while(x == y) {
+
+                System.out.println("looping");
+
+            }
+
+            System.out.println("done");
+
+
+    }
+}
---
+++ Week 3/test.java
@@ -0,0 +1,16 @@
+package test;
+
+
+public static void main(String[] args){
+
+Integer x = 200;
+Integer y = 200;
+while(x == y) {
+
+  System.out.println("looping");
+
+}
+
+System.out.println("done");
+
+}
